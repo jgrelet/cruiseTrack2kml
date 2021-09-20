3f2a54119513b08f4da71e3d4d26ba0c362d3dc6
Jacques Grelet <jacques.grelet@gmail.cpm>
2021-09-20 22:51:50 +0200

    format code

--- python2kml.py
+++ python2kml.py
@@ -1,15 +1,20 @@
 # https://www.sigterritoires.fr/index.php/kml2-comment-creer-des-bulles-ballons-personnalisees/
 from netCDF4 import Dataset
 import simplekml
+
 file = 'data/amazomix/OS_AMAZOMIX_CTD.nc'
-kml = simplekml.Kml()
+
 nc = Dataset(file, mode='r')
 profiles = nc.variables['PROFILE'][:].tolist()
 
+kml = simplekml.Kml()
+
 for i in range(1, len(profiles)):         
-    pnt = kml.newpoint(name="St{:05d}".format(profiles[i]), coords=[(nc.variables['LONGITUDE'][i],
-        nc.variables['LATITUDE'][i])])  # lon, lat, optional height
-    pnt.description = "Station: {:05d}".format(profiles[i])
+    point = kml.newpoint()
+    point.name="St{:05d}".format(profiles[i])
+    point.description = "Station: {:05d}".format(profiles[i])
+    point.coords=[(nc.variables['LONGITUDE'][i], nc.variables['LATITUDE'][i])]
+    
 kml.save("amazomix.kml")
 print(kml.kml())
 nc.close()
